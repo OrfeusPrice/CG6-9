0c580b1969c8991b08dc394ba1aae016c9291135
Orfeus <orfeusprice@gmail.com>
2024-11-12 18:43:08 +0300

    Lab7 added

---
+++ Lab7/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Lab6_9.Polyhedron;
+using static Lab6_9.GeometryAndMatrix;
+
+namespace Lab6_9
+{
+    public partial class Form1 : Form
+    {
+        Bitmap _bm;
+        Graphics _g;
+
+        Object3D _obj;
+        public static float[][] MChanges;
+
+        bool _isPerspective = true;
+        public static bool _isNewObj = false;
+        public static int _tCount;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _bm = new Bitmap(pictureBox.Width, pictureBox.Height);
+            pictureBox.Image = _bm;
+            _g = Graphics.FromImage(pictureBox.Image);
+            _g.Clear(Color.White);
+            _g.TranslateTransform(pictureBox.ClientSize.Width / 2, pictureBox.ClientSize.Height / 2);
+            _g.ScaleTransform(1, -1);
+
+            _isNewObj = false;
+            _tCount = 0;
+            InitMChanges();
+
+            Tetrahedron(ref _obj, 100);
+
+            _obj.Vertexes = _obj.Vertexes.Select(p => TranslatePoint(p, 0, 0, 0)).ToList();
+            DrawObject(_obj);
+        }
+
+        public void InitMChanges()
+        {
+            MChanges = new float[4][]
+            {
+                new float[4] { 1, 0, 0, 0 },
+                new float[4] { 0, 1, 0, 0 },
+                new float[4] { 0, 0, 1, 0 },
+                new float[4] { 0, 0, 0, 1 },
+            };
+        }
+
+        public void DrawObject(Object3D _obj)
+        {
+            List<Point3D> vertexes = _obj.Vertexes;
+
+            if (_isNewObj)
+            {
+                vertexes = vertexes.Select(p => MultiplyMatrix(MChanges, p)).ToList();
+                _isNewObj = false;
+            }
+
+            if (_isPerspective) vertexes = vertexes.Select(p => Perspective(p)).ToList();
+            else vertexes = vertexes.Select(p => Axonometric(p)).ToList();
+
+            foreach (Point3D p in vertexes)
+            {
+                _g.DrawRectangle(new Pen(Color.Red), p.X - 1, p.Y - 1, 2, 2);
+            }
+
+            foreach (Face face in _obj.Faces)
+            {
+                for (int i = 0; i < face.VertexIndexes.Count; i++)
+                {
+                    Point3D p1 = vertexes[face.VertexIndexes[i]];
+                    Point3D p2 = vertexes[face.VertexIndexes[(i + 1) % face.VertexIndexes.Count]];
+                    _g.DrawLine(new Pen(Color.Black),
+                        p1.X,
+                        p1.Y,
+                        p2.X,
+                        p2.Y);
+
+                }
+            }
+        }
+
+        public Point3D Perspective(Point3D p)
+        {
+            float c = -pictureBox.Width;
+
+            float[][] PerspectiveMatrix = new float[4][]
+            {
+                    new float[4] { 1, 0, 0, 0},
+                    new float[4] { 0, 1, 0, 0 },
+                    new float[4] { 0, 0, 0, -1/c },
+                    new float[4] { 0, 0, 0, 1 }
+            };
+
+            _g.DrawLine(new Pen(Color.Blue, 2), 1000, 0, 0, 0); //X
+            _g.DrawLine(new Pen(Color.Green, 2), 0, 1000, 0, 0); //Y
+
+
+            Point3D temp = MultiplyMatrix(PerspectiveMatrix, p);
+            return new Point3D(p.X / temp.W, p.Y / temp.W, 0, p.W);
+        }
+
+        public Point3D Axonometric(Point3D p)
+        {
+            float phi = (float)((35.26 / 180D) * Math.PI); ;
+            float psi = (float)((45 / 180D) * Math.PI); ;
+
+            float[][] AxonometricMatrix = new float[4][]
+            {
+                    new float[4] { (float)Math.Cos(psi), (float)(Math.Sin(phi) * Math.Sin(psi)), 0, 0},
+                    new float[4] { 0, (float)Math.Cos(phi), 0, 0 },
+                    new float[4] { (float)Math.Sin(psi), -(float)(Math.Sin(phi) * Math.Cos(psi)), 0, 0 },
+                    new float[4] { 0, 0, 0, 1 }
+            };
+
+            _g.DrawLine(new Pen(Color.Green, 2), 0, 0, 500, -300); // Z
+            _g.DrawLine(new Pen(Color.Red, 2), 0, 1000, 0, 0); // Y
+            _g.DrawLine(new Pen(Color.Blue, 2), 0, 0, 500, 300); //X
+            //_g.DrawLine(new Pen(Color.LightGreen, 1), 0, 0, -500, 300); // Z
+            //_g.DrawLine(new Pen(Color.Pink, 1), 0, -1000, 0, 0); // Y
+            //_g.DrawLine(new Pen(Color.AliceBlue, 1), 0, 0, -500, -300); //X
+
+            return MultiplyMatrix(AxonometricMatrix, p);
+        }
+
+
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked) _isPerspective = true;
+            if (radioButton2.Checked) _isPerspective = false;
+
+            _g.Clear(Color.White);
+            DrawObject(_obj);
+            pictureBox.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float dx, dy, dz;
+            if (float.TryParse(textBox1.Text, out dx) && float.TryParse(textBox2.Text, out dy) && float.TryParse(textBox3.Text, out dz))
+            {
+                _tCount = 0;
+                _obj.Vertexes = _obj.Vertexes.Select(p => TranslatePoint(p, dx, dy, dz)).ToList();
+
+                _g.Clear(Color.White);
+                DrawObject(_obj);
+                pictureBox.Refresh();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            float angle;
+            if (float.TryParse(textBox4.Text, out angle))
+            {
+                _tCount = 0;
+                if (radioButton3.Checked) _obj.Vertexes = _obj.Vertexes.Select(p => XRotatePoint(p, angle)).ToList();
+                if (radioButton4.Checked) _obj.Vertexes = _obj.Vertexes.Select(p => YRotatePoint(p, angle)).ToList();
+                if (radioButton5.Checked) _obj.Vertexes = _obj.Vertexes.Select(p => ZRotatePoint(p, angle)).ToList();
+
+                _g.Clear(Color.White);
+                DrawObject(_obj);
+                pictureBox.Refresh();
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            float mx, my, mz;
+            if (float.TryParse(textBox5.Text, out mx) && float.TryParse(textBox6.Text, out my) && float.TryParse(textBox7.Text, out mz))
+            {
+                _tCount = 0;
+                _obj.Vertexes = _obj.Vertexes.Select(p => ScalePoint(p, mx, my, mz)).ToList();
+
+                _g.Clear(Color.White);
+                DrawObject(_obj);
+                pictureBox.Refresh();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            _tCount = 0;
+            if (radioButton6.Checked) _obj.Vertexes = _obj.Vertexes.Select(p => XYMirrorPoint(p)).ToList();
+            if (radioButton7.Checked) _obj.Vertexes = _obj.Vertexes.Select(p => XZMirrorPoint(p)).ToList();
+            if (radioButton8.Checked) _obj.Vertexes = _obj.Vertexes.Select(p => YZMirrorPoint(p)).ToList();
+
+            _g.Clear(Color.White);
+            DrawObject(_obj);
+            pictureBox.Refresh();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            float mx, my, mz;
+            if (float.TryParse(textBox8.Text, out mx) && float.TryParse(textBox9.Text, out my) && float.TryParse(textBox10.Text, out mz))
+            {
+                _tCount = 0;
+                GeometryAndMatrix.Scale(ref _obj, mx, my, mz);
+
+                _g.Clear(Color.White);
+                DrawObject(_obj);
+                pictureBox.Refresh();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            float angle;
+            if (float.TryParse(textBox11.Text, out angle))
+            {
+                _tCount = 0;
+                if (radioButton9.Checked) XRotate(ref _obj, angle);
+                if (radioButton10.Checked) YRotate(ref _obj, angle);
+                if (radioButton11.Checked) ZRotate(ref _obj, angle);
+
+                _g.Clear(Color.White);
+                DrawObject(_obj);
+                pictureBox.Refresh();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            float x1, y1, z1, x2, y2, z2, angle;
+            if (float.TryParse(textBox12.Text, out x1) && float.TryParse(textBox13.Text, out y1) && float.TryParse(textBox14.Text, out z1) &&
+                float.TryParse(textBox15.Text, out x2) && float.TryParse(textBox16.Text, out y2) && float.TryParse(textBox17.Text, out z2) &&
+                float.TryParse(textBox18.Text, out angle))
+            {
+                _tCount = 0;
+                Rotate(ref _obj, new Point3D(x1, y1, z1), new Point3D(x2, y2, z2), angle);
+
+                _g.Clear(Color.White);
+                DrawObject(_obj);
+                pictureBox.Refresh();
+            }
+        }
+
+        private void B_Create_Click(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    Tetrahedron(ref _obj, 100);
+                    break;
+                case 1:
+                    Hexahedron(ref _obj, 100);
+                    break;
+                case 2:
+                    Octahedron(ref _obj, 100);
+                    break;
+                case 3:
+                    Icosahedron(ref _obj, 100);
+                    break;
+                case 4:
+                    Dodecahedron(ref _obj, 100);
+                    break;
+                default:
+                    break;
+            }
+            InitMChanges();
+
+            _isNewObj = true;
+            _obj.Vertexes = _obj.Vertexes.Select(p => TranslatePoint(p, 0, 0, 0)).ToList();
+            _g.Clear(Color.White);
+            DrawObject(_obj);
+            pictureBox.Refresh();
+        }
+    }
+}
+
---
+++ Lab7/Polyhedron.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_9
+{
+    internal class Polyhedron
+    {
+            public static void Dodecahedron(ref Object3D obj, float a)
+            {
+                Object3D temp = new Object3D();
+                Icosahedron(ref temp, 1);
+
+                obj = new Object3D();
+
+                foreach (Face f in temp.Faces)
+                {
+                    Point3D sum = new Point3D(0, 0, 0);
+                    foreach (int i in f.VertexIndexes)
+                    {
+                        sum += temp.Vertexes[i];
+                    }
+                    obj.Vertexes.Add(new Point3D(a * sum.X / 3, a * sum.Y / 3, a * sum.Z / 3));
+                }
+
+                float k = a / (float)Math.Sqrt(Math.Pow(obj.Vertexes[0].X - obj.Vertexes[1].X, 2) + Math.Pow(obj.Vertexes[0].Y - obj.Vertexes[1].Y, 2) + Math.Pow(obj.Vertexes[0].Z - obj.Vertexes[1].Z, 2));
+                for (int i = 0; i < obj.Vertexes.Count; i++)
+                    obj.Vertexes[i] = obj.Vertexes[i] * k;
+
+                obj.Faces.Add(new Face(0, 1, 2, 3, 4));
+                obj.Faces.Add(new Face(5, 6, 7, 8, 9));
+
+                obj.Faces.Add(new Face(0, 1, 12, 11, 10));
+                obj.Faces.Add(new Face(1, 2, 14, 13, 12));
+                obj.Faces.Add(new Face(2, 3, 16, 15, 14));
+                obj.Faces.Add(new Face(3, 4, 18, 17, 16));
+                obj.Faces.Add(new Face(4, 0, 10, 19, 18));
+
+                obj.Faces.Add(new Face(5, 6, 13, 12, 11));
+                obj.Faces.Add(new Face(6, 7, 15, 14, 13));
+                obj.Faces.Add(new Face(7, 8, 17, 16, 15));
+                obj.Faces.Add(new Face(8, 9, 19, 18, 17));
+                obj.Faces.Add(new Face(9, 5, 11, 10, 19));
+
+            }
+
+            public static void Icosahedron(ref Object3D obj, float a)
+            {
+                float R = a / (2 * (float)Math.Sin(Math.PI / 6));
+                float r = R * (float)Math.Cos(Math.PI / 6);
+
+                float step = 360 / 5;
+
+                obj = new Object3D();
+
+                obj.Vertexes.Add(new Point3D(0, 0, R));
+                for (float angle = 0; angle < 360; angle += step)
+                {
+                    obj.Vertexes.Add(new Point3D(R * (float)Math.Cos(angle / 180D * Math.PI), R * (float)Math.Sin(angle / 180D * Math.PI), a / 2));
+                }
+
+                obj.Vertexes.Add(new Point3D(0, 0, -R));
+                for (float angle = step / 2; angle < 360; angle += step)
+                {
+                    obj.Vertexes.Add(new Point3D(R * (float)Math.Cos(angle / 180D * Math.PI), R * (float)Math.Sin(angle / 180D * Math.PI), -a / 2));
+                }
+
+                obj.Faces.Add(new Face(0, 1, 2));
+                obj.Faces.Add(new Face(0, 2, 3));
+                obj.Faces.Add(new Face(0, 3, 4));
+                obj.Faces.Add(new Face(0, 4, 5));
+                obj.Faces.Add(new Face(0, 5, 1));
+
+                obj.Faces.Add(new Face(6, 7, 8));
+                obj.Faces.Add(new Face(6, 8, 9));
+                obj.Faces.Add(new Face(6, 9, 10));
+                obj.Faces.Add(new Face(6, 10, 11));
+                obj.Faces.Add(new Face(6, 11, 7));
+
+                obj.Faces.Add(new Face(1, 2, 7));
+                obj.Faces.Add(new Face(7, 8, 2));
+                obj.Faces.Add(new Face(2, 3, 8));
+                obj.Faces.Add(new Face(8, 9, 3));
+                obj.Faces.Add(new Face(3, 4, 9));
+                obj.Faces.Add(new Face(9, 10, 4));
+                obj.Faces.Add(new Face(4, 5, 10));
+                obj.Faces.Add(new Face(10, 11, 5));
+                obj.Faces.Add(new Face(5, 1, 11));
+                obj.Faces.Add(new Face(11, 7, 1));
+
+            }
+
+            public static void Hexahedron(ref Object3D obj, float a)
+            {
+                float R = a / (2 * (float)Math.Sin(Math.PI / 4));
+                float r = R * (float)Math.Cos(Math.PI / 4);
+
+                obj = new Object3D();
+
+                obj.Vertexes.Add(new Point3D(r, r, -r));
+                obj.Vertexes.Add(new Point3D(-r, r, -r));
+                obj.Vertexes.Add(new Point3D(-r, -r, -r));
+                obj.Vertexes.Add(new Point3D(r, -r, -r));
+                obj.Vertexes.Add(new Point3D(r, r, r));
+                obj.Vertexes.Add(new Point3D(-r, r, r));
+                obj.Vertexes.Add(new Point3D(-r, -r, r));
+                obj.Vertexes.Add(new Point3D(r, -r, r));
+
+                obj.Faces.Add(new Face(0, 1, 2, 3));
+                obj.Faces.Add(new Face(4, 5, 6, 7));
+                obj.Faces.Add(new Face(0, 1, 5, 4));
+                obj.Faces.Add(new Face(1, 2, 6, 5));
+                obj.Faces.Add(new Face(2, 3, 7, 6));
+                obj.Faces.Add(new Face(0, 3, 7, 4));
+            }
+
+            public static void Tetrahedron(ref Object3D obj, float a)
+            {
+                float R = a / (2 * (float)Math.Sin(Math.PI / 3));
+                float r = R * (float)Math.Cos(Math.PI / 3);
+
+                obj = new Object3D();
+
+                obj.Vertexes.Add(new Point3D(0, 0, R));
+                obj.Vertexes.Add(new Point3D(R, 0, -r));
+                obj.Vertexes.Add(new Point3D(-r, a / 2, -r));
+                obj.Vertexes.Add(new Point3D(-r, -a / 2, -r));
+
+                obj.Faces.Add(new Face(0, 1, 2));
+                obj.Faces.Add(new Face(0, 2, 3));
+                obj.Faces.Add(new Face(0, 1, 3));
+                obj.Faces.Add(new Face(1, 2, 3));
+            }
+        public static void Octahedron(ref Object3D obj, float a)
+        {
+            obj = new Object3D();
+
+            obj.Vertexes.Add(new Point3D(a, 0, 0)); 
+            obj.Vertexes.Add(new Point3D(-a, 0, 0)); 
+            obj.Vertexes.Add(new Point3D(0, a, 0)); 
+            obj.Vertexes.Add(new Point3D(0, -a, 0)); 
+            obj.Vertexes.Add(new Point3D(0, 0, a)); 
+            obj.Vertexes.Add(new Point3D(0, 0, -a)); 
+
+            obj.Faces.Add(new Face(0, 2, 4)); 
+            obj.Faces.Add(new Face(0, 4, 3)); 
+            obj.Faces.Add(new Face(0, 3, 5));
+            obj.Faces.Add(new Face(0, 5, 2)); 
+
+            obj.Faces.Add(new Face(1, 4, 2)); 
+            obj.Faces.Add(new Face(1, 3, 4)); 
+            obj.Faces.Add(new Face(1, 5, 3)); 
+            obj.Faces.Add(new Face(1, 2, 5)); 
+        }
+
+    }
+    }
